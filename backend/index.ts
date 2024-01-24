6bdb5ae4a74feb1943fdb0c3d09fba2482923f81
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2024-01-24 18:28:13 +0100

    connect client with server

--- backend/index.ts
+++ backend/index.ts
@@ -1,47 +1,54 @@
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 
 import jwt from "jsonwebtoken";
 
 import { PrismaClient, User } from "@prisma/client";
 const prisma = new PrismaClient();
 
-import express from 'express';
+import express from "express";
 const app = express();
 
-import { hashPassword, passwordMatches } from './utils/bcrypt';
+import { hashPassword, passwordMatches } from "./utils/bcrypt";
 
 app.use(express.json());
 
 app.use((req: Request, res: Response, next: any) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
     next();
 });
 
+app.get("/", (req: Request, res: Response) => {
+    res.send("Connected to server successfully!");
+});
+
 //test endpoint
-app.get('/test', (req: Request, res: Response) => {
-    res.send('Hello World!');
+app.get("/test", (req: Request, res: Response) => {
+    res.send("Hello test!");
 });
 
 //get all users
-app.get('/users', async (req: Request, res: Response) => {
+app.get("/users", async (req: Request, res: Response) => {
     const users = await prisma.user.findMany();
     res.status(200).json(users);
 });
 
 // insert new user
-app.post('/register', async (req: Request, res: Response) => {
+app.post("/register", async (req: Request, res: Response) => {
     const { name, email, password } = req.body;
-    
+
     try {
         const hashedPassword = await hashPassword(password);
-        const userData = {name: name, email: email, password: hashedPassword};
+        const userData = { name: name, email: email, password: hashedPassword };
 
         const data = await prisma.user.create({
             data: userData,
         });
-        
+
         res.status(201).json(data);
     } catch (err: any) {
         res.status(500).json(err);
@@ -49,7 +56,7 @@ app.post('/register', async (req: Request, res: Response) => {
 });
 
 // login
-app.post('/login', async (req: Request, res: Response) => {
+app.post("/login", async (req: Request, res: Response) => {
     const { email, password } = req.body;
     const user = await prisma.user.findUnique({
         where: {
@@ -58,7 +65,7 @@ app.post('/login', async (req: Request, res: Response) => {
     });
 
     if (!user) {
-        res.status(404).json({message: 'User not found'});
+        res.status(404).json({ message: "User not found" });
         return;
     }
 
@@ -67,15 +74,17 @@ app.post('/login', async (req: Request, res: Response) => {
         const validPassword = await passwordMatches(password, hash);
 
         if (!validPassword) {
-            res.status(401).json({message: 'Invalid password'});
+            res.status(401).json({ message: "Invalid password" });
             return;
         }
 
         //set cookie
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET!, { expiresIn: "7d"})
-        res.cookie("token", token, { httpOnly: true, secure: true, path: "/", });
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET!, {
+            expiresIn: "7d",
+        });
+        res.cookie("token", token, { httpOnly: true, secure: true, path: "/" });
 
-        res.status(200).json({message: 'Login successfull'});
+        res.status(200).json({ message: "Login successfull" });
     } catch (err: any) {
         res.status(500).json(err);
     }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
 export default function Home() {
-  return (
-    <main>
-      letsvote
-    </main>
-  );
+    const [data, setData] = useState("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(
+                    // how to use .env variables on the client side in next.js: https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables
+                    `http://localhost:${process.env.NEXT_PUBLIC_BE_PORT}`
+                );
+                const result = await response.text();
+                setData(result);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, []); // Empty dependency array ensures this runs once on component mount
+
+    return (
+        <main>
+            <h1>letsvote frontend</h1>
+            <p>{data}</p>
+        </main>
+    );
 }
