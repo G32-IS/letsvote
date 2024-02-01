b95c534a1070b0a7a9c7aec5f7e8c1869b2cf8fa
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2024-02-01 15:40:55 +0100

    build slider

--- frontend/app/guide/page.tsx
+++ frontend/app/guide/page.tsx
@@ -1,24 +1,78 @@
+"use client";
+
 import Link from "next/link";
+import React, { useState } from "react";
 
 import styles from "./page.module.css";
 
 export default function Page() {
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const nextPage = () => {
+        setCurrentPage((prevPage) => (prevPage < 5 ? prevPage + 1 : 1));
+    };
+
+    const prevPage = () => {
+        setCurrentPage((prevPage) => (prevPage > 1 ? prevPage - 1 : 5));
+    };
+
     return (
         <main className={styles.main}>
             <h1>Guida Introduttiva</h1>
+            {/* questi soprastanti sono i punti su cui costruire le slide.
+                Gli abbiamo messi nel D2 */}
             <article>
-                <h2>TODO: slides to include</h2>
-                <ul>
-                    <li>lista votazioni</li>
-                    <li>andamento votazioni</li>
-                    <li>partecipazione a votazione</li>
-                    <li>storico votazioni</li>
-                    <li>richiesta amm. delle votazioni</li>
-                </ul>
-                <p>
-                    questi soprastanti sono i punti su cui costruire le slide.
-                    Gli abbiamo messi nel D2
-                </p>
+                <div className={styles.slider}>
+                    <section
+                        className={
+                            currentPage === 1
+                                ? styles.displayBlock
+                                : styles.displayNone
+                        }
+                    >
+                        lista votazioni
+                    </section>
+                    <section
+                        className={
+                            currentPage === 2
+                                ? styles.displayBlock
+                                : styles.displayNone
+                        }
+                    >
+                        andamento votazioni
+                    </section>
+                    <section
+                        className={
+                            currentPage === 3
+                                ? styles.displayBlock
+                                : styles.displayNone
+                        }
+                    >
+                        partecipazione a votazione
+                    </section>
+                    <section
+                        className={
+                            currentPage === 4
+                                ? styles.displayBlock
+                                : styles.displayNone
+                        }
+                    >
+                        storico votazioni
+                    </section>
+                    <section
+                        className={
+                            currentPage === 5
+                                ? styles.displayBlock
+                                : styles.displayNone
+                        }
+                    >
+                        richiesta amm. delle votazioni
+                    </section>
+                </div>
+                <div className={styles.btnsContainer}>
+                    <button onClick={prevPage}>Prev</button>
+                    <button onClick={nextPage}>Next</button>
+                </div>
             </article>
             <footer>
                 To know more about the usage of letsvote, visit the{" "}
