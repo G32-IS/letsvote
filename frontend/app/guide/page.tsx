52cdc82240f683ae83abe526fdd828e49b837c27
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2024-02-01 14:57:31 +0100

    add points on which to build guide slides

--- frontend/app/guide/page.tsx
+++ frontend/app/guide/page.tsx
@@ -6,7 +6,20 @@ export default function Page() {
     return (
         <main className={styles.main}>
             <h1>Guida Introduttiva</h1>
-            <article>content</article>
+            <article>
+                <h2>TODO: slides to include</h2>
+                <ul>
+                    <li>lista votazioni</li>
+                    <li>andamento votazioni</li>
+                    <li>partecipazione a votazione</li>
+                    <li>storico votazioni</li>
+                    <li>richiesta amm. delle votazioni</li>
+                </ul>
+                <p>
+                    questi soprastanti sono i punti su cui costruire le slide.
+                    Gli abbiamo messi nel D2
+                </p>
+            </article>
             <footer>
                 To know more about the usage of letsvote, visit the{" "}
                 <Link href="/guide/api-docs">api-docs</Link> page
