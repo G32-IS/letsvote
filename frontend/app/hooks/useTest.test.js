5f058ceb9cade6080b1e3f11ab07b9c54380b730
Geeeean <violoni.gianluca@gmail.com>
2024-02-07 04:42:49 +0100

    removed useTest.test.js from hooks directory

--- frontend/app/hooks/useTest.test.js
+++
@@ -1,7 +0,0 @@
-import { fetchTest } from "./useTest";
-
-test("the data is peanut butter", () => {
-    return fetchTest().then((data) => {
-        expect(typeof data.count).toBe("number");
-    });
-});
